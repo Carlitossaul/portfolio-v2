076ed26c2dcd1d48a0b9cff2c941604f8a3b59de
Carlitossaul <carlitossaul.h@gmail.com>
2023-07-03 15:44:27 -0300

    corregido el parrafo de about

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,11 +3,16 @@ import RickAndMorty from "../Projects/RickAndMorty";
 import SearchImages from "../Projects/SearchImages";
 import WorkifyServices from "../Projects/WorkifyServices";
 import styles from "./Portfolio.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import imgLoading from "../../assets/loadingport.gif";
 
 const Portfolio = () => {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     window.scrollTo(0, 0);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
   }, []);
 
   return (
@@ -16,13 +21,19 @@ const Portfolio = () => {
         <h1>Portfolio</h1>
         <h4>Check out my latest projects:</h4>
       </div>
-      <div className={styles.projectsContainer}>
-        <AreYouHungry />
-        <WorkifyServices />
-        <SearchImages />
-        <RickAndMorty />
-        <div className={styles.space}></div>
-      </div>
+      {loading ? (
+        <div className={styles.loadingContainer}>
+          <img className={styles.loading} src={imgLoading} alt="loading" />
+        </div>
+      ) : (
+        <div className={styles.projectsContainer}>
+          <AreYouHungry />
+          <WorkifyServices />
+          <SearchImages />
+          <RickAndMorty />
+          <div className={styles.space}></div>
+        </div>
+      )}
     </div>
   );
 };

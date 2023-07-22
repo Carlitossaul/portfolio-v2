a9bb235b0f396ab3256dd4f33c730cc9c287d025
Carlitossaul <carlitossaul.h@gmail.com>
2023-07-22 13:03:59 -0300

    lazy loading on App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import imgLoading from "./assets/loadingport.gif";
+import styles from "./App.module.css";
+
+//components views
 import Nav from "./components/Nav/Nav";
-import About from "./components/About/About";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Pdf from "./components/Nav/Pdf.jsx";
-import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Landing from "./components/Landing/Landing";
+
+//components views lazy
+const About = lazy(() => import("./components/About/About"));
+const Portfolio = lazy(() => import("./components/Portfolio/Portfolio"));
+const Pdf = lazy(() => import("./components/Nav/Pdf.jsx"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
 
 function App() {
   return (
     <div>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route exact path="/pdf" element={<Pdf />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className={styles.loadingContainer}>
+            <img className={styles.loading} src={imgLoading} alt="loading" />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route exact path="/pdf" element={<Pdf />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
